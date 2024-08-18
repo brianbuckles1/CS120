e8c5bb8c2c2563464491afe91ff24db3885db6a1
Brian Buckles <brian.buckles@outlook.com>
2024-08-18 16:41:19 -0400

    initial

---
+++ MadLib/README.txt
@@ -0,0 +1,22 @@
+Mad Lib Game
+Author:  Brian Buckles
+Date:    8/18/2024
+
+create a variable vehicle for a vehicle that will be set by prompting the user "Name a vechicle"
+create a varialbe color for a vehicle color that will be set by prompting the user "Please provide a color"
+create a variable sitting_next_to for sitting_next_to that will be set by prompting the user "Name the person, animal or object"
+create a variable clothing for clothing that will be set by prompting the user "Name a type of clothing"
+create a variable footware for footware that will be set by prompting the user "Name a type of footware"
+create a variable drink for a drink that will be set by prompting the user "Provide a drink"
+create a variable dinner for a dinner that will be set by prompting the user for "Name a dinner you enjoy"
+create a variable location for a location that will be set by prompting the user for "Name a location"
+create a variable activity for an activity that will be set by prompting the user for "Name an activity"
+create a variable music_genre for a the first music genre that will be set by prompting the user for "Name a music genre"
+create a variable music_genre_artist for an artist for music genre that will be set by prompting the user for "Name an artist for this genre"
+create a variable music_genre_2 for another music genre that will be set by prompting the user for "Name another music genre"
+create a variable music_genre_2_artist for an artist for the music genre just provided by prompting the user for "Name an artist for this genre"
+create a variable weather_type for weather that will be set by prompting the user for "Name a type of weather"
+create a variable river for a river that will be set by prompting the user for "Name a river"
+create a variable song_lyrics for holding the song lyrics to Luke Bryan's - That's my kind of night lyrics
+replace lyrics using fstring with the variables that were created and set
+output lyrics to the end user
---
+++ MadLib/brian.buckles_madlib.py
@@ -0,0 +1,84 @@
+# Mad Lib Game
+# Brian Buckles
+# 8/18/2024
+
+# variables and inputs
+vehicle = input("Name a vehicle: ")
+color = input("Please provide a color: ")
+sitting_next_to=input("What are you sitting next to: ")
+clothing = input("What kind of clothing do you like: ")
+footware = input("What kind of footware do you like? boots, slippers, sneakers, ...:  ")
+drink = input("What do you like to drink: ")
+dinner=input("What did you have or going to have for dinner: ")
+location = input("Name a location: ")
+activity = input("Name an activity: ")
+music_genre=input("Name a music genre: ")
+music_genre_artist=input(f"Name an artist in {music_genre}: ")
+music_genre_2=input("Name a second music genre: ")
+music_genre_2_artist=input(f"Name a artist in {music_genre_2}: ")
+weather_type=input("Name a type of weather: ")
+river=input("Name a river: ")
+song_lyrics=f"""
+Uh, uh
+I got that real good, feel good stuff
+Up under the seat of my big {color} jacked up {vehicle}
+Rollin' on thirty-fives
+{sitting_next_to} by my side
+You got that sun tan {clothing} and {footware}
+Waitin' on you to look my way and scoot
+Your little hot self over here
+Girl, hand me another {drink}, yeah
+All them other boys wanna wine you up and take you {location}
+But you look like the kind that likes to {activity}
+Out where the corn rows grow, row, row my boat
+Floatin' down the {river} River
+Catch us up a little {dinner} dinner
+Gonna sound like a winner
+When I lay you down and love you right
+Yeah, that's my kind of night
+(Oh)
+Might sit down on my diamond plate tailgate
+Put in my {music_genre} {music_genre_2} mixtape
+Little {music_genre_artist}, a little {music_genre_2_artist}
+Might just make it {weather_type} (make it {weather_type})
+You can hang your {clothing} on a limb
+Hit that bank and we can ease on in
+Soak us up a little moonlight
+You know I know what you like, yeah
+All them other boys wanna wine you up and take you {location}
+But you look like the kind that likes to {activity}
+Out where the corn rows grow, row, row my boat
+Floatin' down the {river} River
+Catch us up a little {dinner} dinner
+Gonna sound like a winner
+When I lay you down and love you right
+Yeah, that's my kind of night
+Yeah, that's my kind of night
+(Oh)
+My kind or your kind, it's this kind of night
+We dance in the dark and your lips land on mine
+Oh, gonna get our love on
+Oh, time to get our buzz on
+All them other boys wanna wine you up and take you {location}
+But you look like the kind that likes to {activity}
+Out where the corn rows grow, row, row my boat
+Floatin' down the {river} River
+Catch us up a little {dinner} dinner
+Gonna sound like a winner
+When I lay you down and love you right
+Yeah, that's my kind of night
+Yeah, that's my kind of night
+(Oh)
+Yeah, that's my kind of night
+(Oh)
+Yeah, that's my kind of night
+(Oh)
+(Oh) come on
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Source: LyricFind
+Songwriters: Ashley Glenn Gorley / Christopher Michael De Stefano / Gerald Dallas Davidson
+That's My Kind of Night lyrics © Downtown Music Publishing, Royalty Network, Sony/ATV Music Publishing LLC, Warner Chappell Music, Inc
+"""
+
+#output the final result to the user
+print(song_lyrics)
