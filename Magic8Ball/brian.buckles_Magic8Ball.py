d2adbcd22115bd5df6ed4ccad6104e0628c4d369
brianbuckles1 <brian.buckles@outlook.com>
2024-08-31 20:50:42 -0400

    fixed bad selection

--- Magic8Ball/brian.buckles_Magic8Ball.py
+++ Magic8Ball/brian.buckles_Magic8Ball.py
@@ -13,7 +13,7 @@ print("3 Ask your question for your fortune")
 menu_selection = input("What is your numberic selection: ")
 
 # if menu selection is 1 print out fortunes
-if menu_selection == "1":
+if menu_selection[0] == "1":
     # loop and print out the fortunes
     for (index,fortune) in enumerate(fortunes):
         print(f"{index} - {fortune}")
@@ -21,7 +21,7 @@ if menu_selection == "1":
     exit()
 
 # if menu selection is 2 we need to get the index for the fortune the user would like to see.
-if menu_selection == "2":
+if menu_selection[0] == "2":
     specific_fortune = input(f"What is the index for fortune would you like to see? 0 - {len(fortunes) - 1}: ")
 
     # check if specific fortune is numeric
@@ -40,7 +40,7 @@ if menu_selection == "2":
     exit()
 
 # if menu selection is 3 we need to get the user question and generate the response.
-if menu_selection == "3":
+if menu_selection[0] == "3":
     # get users question
     question = input("What question would you like to ask: ")
     # display the question back to the user.
@@ -49,4 +49,6 @@ if menu_selection == "3":
     random_int = random.randint(0, len(fortunes)-1)
     # print out the fortune for the random_int
     print(f"{fortunes[random_int]}")
+    exit()
 
+print("shucks, well you didn't follow my directions and put in a valid menu selection.")
