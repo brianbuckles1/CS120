2180095feed38067878aac28143dc712ed757d14
brianbuckles1 <brian.buckles@outlook.com>
2024-11-02 16:03:00 -0400

    fixed main script.  removed the pieces not needed yet.

--- SlideAndCatch/main.py
+++ SlideAndCatch/main.py
@@ -1,12 +1,9 @@
 import scenes.game
 
 def main():
-    keepGoing = True
-    bestScore = 0
     game = scenes.game.Game((800,600))
-
-    while keepGoing:
-        game.start()
+    game.start()
 
 
-main()
+if __name__ == "__main__":
+    main()
