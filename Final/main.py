5a7dd476e4e5b914c3816b7c639d7319537c7b0c
brianbuckles1 <brian.buckles@outlook.com>
2024-12-02 20:08:16 -0500

    added save and load for saving data.

--- Final/main.py
+++ Final/main.py
@@ -1,8 +1,14 @@
+import json
+
 import scenes.game, scenes.menu
 
 def main():
     keepGoing = True
     high_score = 0
+
+    saveData = loadSave()
+    if saveData != []:
+        high_score = int(saveData["high_score"])
 
     while keepGoing:
         main_menu = scenes.menu.MainMenu((800, 400), high_score)
@@ -13,13 +19,35 @@ def main():
             game.start()
 
             if game.get_score() > high_score:
-                print("setting new high score")
                 high_score = game.get_score()
-                print(high_score)
+                save(high_score)
         elif main_menu.get_command() == "quit":
             keepGoing = False
 
+def loadSave():
+    """
+    Load the save file
+    """
+    saveData = []
 
+    try:
+        with open("save.dat", "r") as file:
+            saveData = json.load(file, encoding="utf-8")
+            print(f"Save file loaded {saveData}")
+    except FileNotFoundError:
+        print("No save file found")
+
+    return saveData
+
+def save(high_score:int):
+    """
+    Save the high score
+    """
+    saveFile = {
+        "high_score": high_score
+    }
+    with open("save.dat", "w") as file:
+        json.dump(saveFile, file)
 
 if __name__ == "__main__":
     main()
